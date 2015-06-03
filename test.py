6157de18e488df27049ed2f7daa44caad5f4324c
Christian Glöckner <cgloeckner@freenet.de>
2015-06-03 15:30:10 +0200

    Made plugin ctor more flexible + Modified unit testing

--- test.py
+++ test.py
@@ -37,11 +37,18 @@ class RedisTest(unittest.TestCase):
         self.app({'PATH_INFO':'/2', 'REQUEST_METHOD':'GET'}, lambda x, y: None)
     
     def test_optional_args(self):
-        self.plugin = self.app.install(redis_plugin.Plugin(database=1))
+        self.plugin = self.app.install(redis_plugin.Plugin(db=1,
+                                                           decode_responses=True))
 
         @self.app.get('/db/1')
         def test_db_arg(rdb):
             self.assertTrue(rdb.connection_pool.connection_kwargs['db'] == 1)
+            self.assertTrue(rdb.connection_pool.connection_kwargs['decode_responses'] == True)
+            rdb.set('test', 'bottle')
+            if py3k:
+                self.assertEqual(rdb.get('test'), 'bottle')
+            else:
+                self.assertEqual(rdb.get('test'), u'bottle')
         self.app({'PATH_INFO':'/db/1', 'REQUEST_METHOD':'GET'},
                  lambda x,y: None)
         
