2c6b0cfc2c69b4c683e7c7a317e9b1f7a1a411a8
Thiago Avelino <thiagoavelinoster@gmail.com>
2015-06-03 15:39:11 -0300

    fix py3k support (3.2) #8

--- test.py
+++ test.py
@@ -46,9 +46,9 @@ class RedisTest(unittest.TestCase):
             self.assertTrue(rdb.connection_pool.connection_kwargs['decode_responses'] == True)
             rdb.set('test', 'bottle')
             if py3k:
-                self.assertEqual(rdb.get('test'), 'bottle')
+                self.assertEqual(rdb.get('test'), b'bottle')
             else:
-                self.assertEqual(rdb.get('test'), u'bottle')
+                self.assertEqual(rdb.get('test'), 'bottle')
         self.app({'PATH_INFO':'/db/1', 'REQUEST_METHOD':'GET'},
                  lambda x,y: None)
         
